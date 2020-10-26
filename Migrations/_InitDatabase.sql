eed9a844f8ae4006c3630754d2c8322f59d8649a
Oleg Bruev <job4bruev@gmail.com>
2020-10-26 17:27:21 +0300

    1.0.0.ALPHA.2

--- Migrations/_InitDatabase.sql
+++ Migrations/_InitDatabase.sql
@@ -7,7 +7,7 @@ delete from dbo.AspNetUsers;
 insert into dbo.AspNetUsers (id,UserName,NormalizedUserName,Email,NormalizedEmail,EmailConfirmed,PasswordHash,
 								SecurityStamp,ConcurrencyStamp,PhoneNumber,PhoneNumberConfirmed,TwoFactorEnabled, LockoutEnd,LockoutEnabled,AccessFailedCount,Title) 
 								values ('4F9415DE-E69F-4412-B508-4C20C620DDDE','Admin','ADMIN','no email','NO EMAIL',1,'AQAAAAEAACcQAAAAEOn1si4RHnIcKIiisyWetWE4uYxV6eZ0KZspSBFuoFph+OZJYCFW1ReR6uBfm1YN4g==',
-								'F2POTDITTIO6AJHBL5Z7PA6USV7DYE3I','47524004-0a26-4315-a111-7b61f6138031',null,0,0,null,1,0,'Administarator');
+								'F2POTDITTIO6AJHBL5Z7PA6USV7DYE3I','47524004-0a26-4315-a111-7b61f6138031',null,0,0,null,1,0,'Administrator');
 
 delete from dbo.AspNetUserRoles;
 insert into dbo.AspNetUserRoles (UserId,RoleId) values('4F9415DE-E69F-4412-B508-4C20C620DDDE','A0EE3785-9481-4DA2-BBC3-81CEDBDE93EF');
